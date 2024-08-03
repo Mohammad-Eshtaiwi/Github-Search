8fa07b67a1de9b608089a1e7a616efaeca4d4e28
LetMeGuideU <53796803+LetMeGuideU@users.noreply.github.com>
2024-08-03 20:10:54 +0300

    cleared try catch since I want to the error to be caught by react query

--- src/services/getRepositories.ts
+++ src/services/getRepositories.ts
@@ -6,20 +6,13 @@ export default async function getRepositories(
   newLink: string | null = null
 ) {
   const url = `https://api.github.com/search/repositories?q=${query}&per_page=27&page=1`;
-
-  try {
-    const response = await fetch(newLink || url);
-
-    if (!response.ok) {
-      throw new Error(`Error: ${response.status} ${response.statusText}`);
-    }
-
-    const repositories: repositorySearchResponse = await response.json();
-    const linkHeader = response.headers.get("link");
-    const nextLink = linkHeader ? extractNextPage(linkHeader) : null;
-    return { repositories, nextLink };
-  } catch (error) {
-    console.error("Error:", error);
-    return {};
+  const response = await fetch(newLink || url);
+  if (!response.ok) {
+    throw new Error(`Error: ${response.status} ${response.statusText}`);
   }
+
+  const repositories: repositorySearchResponse = await response.json();
+  const linkHeader = response.headers.get("link");
+  const nextLink = linkHeader ? extractNextPage(linkHeader) : null;
+  return { repositories, nextLink };
 }
--- src/services/getRepositoryRelatedData.ts
+++ src/services/getRepositoryRelatedData.ts
@@ -1,23 +1,18 @@
 import repositoryRelatedDataType from "@/types/repositoryRelatedData";
 
 export default async function getRepositoryRelatedData(urls: string[]) {
-  try {
-    const fetchRequests = urls.map((url) => fetch(url));
+  const fetchRequests = urls.map((url) => fetch(url));
 
-    const responses = await Promise.all(fetchRequests);
+  const responses = await Promise.all(fetchRequests);
 
-    const repositoryRelatedData = await Promise.all(
-      responses.map((response) => {
-        if (!response.ok) {
-          throw new Error(`Error: ${response.status} ${response.statusText}`);
-        }
-        return response.json();
-      })
-    );
+  const repositoryRelatedData = await Promise.all(
+    responses.map((response) => {
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status} ${response.statusText}`);
+      }
+      return response.json();
+    })
+  );
 
-    return repositoryRelatedData as repositoryRelatedDataType;
-  } catch (error) {
-    console.error("Error:", error);
-    return {};
-  }
+  return repositoryRelatedData as repositoryRelatedDataType;
 }
